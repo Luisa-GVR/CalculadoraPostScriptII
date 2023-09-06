8a611361c74e1052a980c83709cf7ff63532c941
Iosaf-uwu <116690709+Iosaf-uwu@users.noreply.github.com>
2023-09-05 20:23:23 -0700

    quit pstack pop

--- Main.java
+++ Main.java
@@ -6,80 +6,94 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        boolean exit = true;
 
-        System.out.println("Escribe el calculo en postcript");
-        String input = sc.nextLine();
+        while (exit){
+            System.out.println("Escribe el calculo en postcript");
+            String input = sc.nextLine();
 
-        String[] stackSentence = input.split(" ");
+            String[] stackSentence = input.split(" ");
 
-        for (int i = 0; i < stackSentence.length; i++) {
-            stackSentence[i] = stackSentence[i].toLowerCase();
+            for (int i = 0; i < stackSentence.length; i++) {
+                stackSentence[i] = stackSentence[i].toLowerCase();
+            }
+
+            Stack<String> stringStack = new Stack<>();
+            boolean entrar;
+            double var1;
+            double var2;
+
+            for (String word : stackSentence) {
+                entrar = true; //reset
+
+                if (word.equals("add")) { //suma
+                    entrar = false;
+                    var1 = Double.parseDouble(stringStack.pop());
+                    var2 = Double.parseDouble(stringStack.pop());
+                    stringStack.push(String.valueOf(var1 + var2));
+
+                }
+                if (word.equals("sub")) { //resta
+                    entrar = false;
+                    var1 = Double.parseDouble(stringStack.pop());
+                    var2 = Double.parseDouble(stringStack.pop());
+                    stringStack.push(String.valueOf(var2 - var1));
+
+                }
+
+                if (word.equals("div")) { //division
+                    entrar = false;
+                    var1 = Double.parseDouble(stringStack.pop());
+                    var2 = Double.parseDouble(stringStack.pop());
+                    stringStack.push(String.valueOf(var1 ));
+
+                }
+
+                if (word.equals("mul")) { //multiplicacion
+                    entrar = false;
+                    var1 = Double.parseDouble(stringStack.pop());
+                    var2 = Double.parseDouble(stringStack.pop());
+                    stringStack.push(String.valueOf(var1 * var2));
+
+                }
+
+                if (word.equals("dup")) { //duplicar
+                    entrar = false;
+                    var1 = Double.parseDouble(stringStack.pop());
+                    stringStack.push(String.valueOf(var1));
+                    stringStack.push(String.valueOf(var1));
+                }
+
+                if (word.equals("exch")) { //intercambiar
+                    entrar = false;
+                    var1 = Double.parseDouble(stringStack.pop());
+                    var2 = Double.parseDouble(stringStack.pop());
+                    stringStack.push(String.valueOf(var1));
+                    stringStack.push(String.valueOf(var2));
+                }
+
+                if (word.equals("pop")) { //pop
+                    stringStack.pop();
+                }
+                if (word.equals("pstack")){
+
+                    System.out.println(stringStack.pop());
+                }
+
+                if (word.equals("quit")){
+                    System.exit(3);
+                }
+
+                if (entrar) {
+                    stringStack.push(word);
+
+                }
+
+            }
         }
 
-        Stack<String> stringStack = new Stack<>();
-        boolean entrar;
-        double var1;
-        double var2;
-
-        for (String word : stackSentence) {
-            entrar = true; //reset
-
-            if (word.equals("add")) { //suma
-                entrar = false;
-                var1 = Double.parseDouble(stringStack.pop());
-                var2 = Double.parseDouble(stringStack.pop());
-                stringStack.push(String.valueOf(var1 + var2));
-
-            }
-            if (word.equals("sub")) { //resta
-                entrar = false;
-                var1 = Double.parseDouble(stringStack.pop());
-                var2 = Double.parseDouble(stringStack.pop());
-                stringStack.push(String.valueOf(var2 - var1));
-
-            }
-
-            if (word.equals("div")) { //division
-                entrar = false;
-                var1 = Double.parseDouble(stringStack.pop());
-                var2 = Double.parseDouble(stringStack.pop());
-                stringStack.push(String.valueOf(var2 / var1 ));
-
-            }
-
-            if (word.equals("mul")) { //multiplicacion
-                entrar = false;
-                var1 = Double.parseDouble(stringStack.pop());
-                var2 = Double.parseDouble(stringStack.pop());
-                stringStack.push(String.valueOf(var1 * var2));
-
-            }
-
-            if (word.equals("dup")) { //duplicar
-                entrar = false;
-                var1 = Double.parseDouble(stringStack.pop());
-                stringStack.push(String.valueOf(var1));
-                stringStack.push(String.valueOf(var1));
-            }
-
-            if (word.equals("exch")) { //intercambiar
-                entrar = false;
-                var1 = Double.parseDouble(stringStack.pop());
-                var2 = Double.parseDouble(stringStack.pop());
-                stringStack.push(String.valueOf(var1));
-                stringStack.push(String.valueOf(var2));
-            }
 
 
-            if (entrar) {
-                stringStack.push(word);
-
-            }
-
-
-        }
-
-        System.out.println(stringStack.pop());
     }
 
 }
