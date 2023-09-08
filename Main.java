1b81cee8133951c8eb4c1d9236ee380fdc2394cb
angel <angel.zazueta14@hotmail.com>
2023-09-08 10:28:33 -0700

    Def eq

--- Main.java
+++ Main.java
@@ -1,99 +1,154 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         boolean exit = true;
+        Stack<String> stringStack = new Stack<>();
 
-        while (exit){
+        boolean entrar;
+        boolean definiendoValor = false;
+        boolean valorYaDefinido = false;
+        double valorDefinido = 0;
+        String nombreDefinicion = "";
+        double var1;
+        double var2;
+        HashMap<String, Double> definiciones = new HashMap<>();
+        HashMap<String, Double> tempDefiniciones = new HashMap<>();
+
+        boolean valorNoAceptado = false;
+
+        while (exit) {
             System.out.println("Escribe el calculo en postcript");
             String input = sc.nextLine();
 
             String[] stackSentence = input.split(" ");
 
+
             for (int i = 0; i < stackSentence.length; i++) {
                 stackSentence[i] = stackSentence[i].toLowerCase();
             }
 
-            Stack<String> stringStack = new Stack<>();
-            boolean entrar;
-            double var1;
-            double var2;
 
             for (String word : stackSentence) {
                 entrar = true; //reset
 
-                if (word.equals("add")) { //suma
-                    entrar = false;
-                    var1 = Double.parseDouble(stringStack.pop());
-                    var2 = Double.parseDouble(stringStack.pop());
-                    stringStack.push(String.valueOf(var1 + var2));
+                if (!definiendoValor) {
+                    if (word.equals("add")) { //suma
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        var2 = Double.parseDouble(stringStack.pop());
+                        stringStack.push(String.valueOf(var1 + var2));
 
+                    }
+                    if (word.equals("sub")) { //resta
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        var2 = Double.parseDouble(stringStack.pop());
+                        stringStack.push(String.valueOf(var2 - var1));
+
+                    }
+
+                    if (word.equals("div")) { //division
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        var2 = Double.parseDouble(stringStack.pop());
+                        stringStack.push(String.valueOf(var1 ));
+
+                    }
+
+                    if (word.equals("mul")) { //multiplicacion
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        var2 = Double.parseDouble(stringStack.pop());
+                        stringStack.push(String.valueOf(var1 * var2));
+
+                    }
+
+                    if (word.equals("dup")) { //duplicar
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        stringStack.push(String.valueOf(var1));
+                        stringStack.push(String.valueOf(var1));
+                    }
+
+                    if (word.equals("exch")) { //intercambiar
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        var2 = Double.parseDouble(stringStack.pop());
+                        stringStack.push(String.valueOf(var1));
+                        stringStack.push(String.valueOf(var2));
+                    }
+
+                    if (word.equals("pop")) { //pop
+                        stringStack.pop();
+                    }
+
+                    if (word.equals("eq")){ // equals
+                        entrar = false;
+                        var1 = Double.parseDouble(stringStack.pop());
+                        var2 = Double.parseDouble(stringStack.pop());
+                        if (var1 == var2){
+                            stringStack.push("true");
+                        } else {
+                            stringStack.push("false");
+                        }
+
+                    }
+
+                    if (word.startsWith("/")){
+                        definiendoValor = true;
+                        nombreDefinicion = word.substring(1, word.length());
+                    }
+
+
+                    if (word.equals("pstack")){ // Imprime
+
+                        if (stringStack.peek() != null) {
+                            System.out.println(stringStack.peek());
+                        } else if (stringStack.peek().equals("pstack")){
+                            System.out.println("No hay valores en la pila");
+                        }
+                    }
+
+                    if (definiciones.containsKey(word)){
+                        System.out.println(definiciones.get(word));
+
+                    }
+
+
+
+                    if (word.equals("quit")){
+                        System.exit(3);
+                    }
+
+                    if (entrar) {
+                        stringStack.push(word);
+
+                    }
+
+
+                } else { //definimos el valor
+                    if (!valorYaDefinido) {
+                        valorDefinido = Double.parseDouble(word);
+                        valorYaDefinido = true;
+                    } else {
+                        if (word.equals("def")) {
+                            definiciones.put(nombreDefinicion, valorDefinido);
+                            valorYaDefinido = false;
+                            definiendoValor = false;
+                        } else {
+                            System.out.println("No agrego 'def' a la hora de definir su valor");
+                            valorYaDefinido = false;
+                            definiendoValor = false;
+                            // Restaurar el HashMap temporal
+                            definiciones.putAll(tempDefiniciones);
+                            tempDefiniciones.clear();
+                        }
+                    }
                 }
-                if (word.equals("sub")) { //resta
-                    entrar = false;
-                    var1 = Double.parseDouble(stringStack.pop());
-                    var2 = Double.parseDouble(stringStack.pop());
-                    stringStack.push(String.valueOf(var2 - var1));
-
-                }
-
-                if (word.equals("div")) { //division
-                    entrar = false;
-                    var1 = Double.parseDouble(stringStack.pop());
-                    var2 = Double.parseDouble(stringStack.pop());
-                    stringStack.push(String.valueOf(var1 ));
-
-                }
-
-                if (word.equals("mul")) { //multiplicacion
-                    entrar = false;
-                    var1 = Double.parseDouble(stringStack.pop());
-                    var2 = Double.parseDouble(stringStack.pop());
-                    stringStack.push(String.valueOf(var1 * var2));
-
-                }
-
-                if (word.equals("dup")) { //duplicar
-                    entrar = false;
-                    var1 = Double.parseDouble(stringStack.pop());
-                    stringStack.push(String.valueOf(var1));
-                    stringStack.push(String.valueOf(var1));
-                }
-
-                if (word.equals("exch")) { //intercambiar
-                    entrar = false;
-                    var1 = Double.parseDouble(stringStack.pop());
-                    var2 = Double.parseDouble(stringStack.pop());
-                    stringStack.push(String.valueOf(var1));
-                    stringStack.push(String.valueOf(var2));
-                }
-
-                if (word.equals("pop")) { //pop
-                    stringStack.pop();
-                }
-                if (word.equals("pstack")){
-
-                    System.out.println(stringStack.pop());
-                }
-
-                if (word.equals("quit")){
-                    System.exit(3);
-                }
-
-                if (entrar) {
-                    stringStack.push(word);
-
-                }
-
             }
         }
-
-
-
     }
-
 }
